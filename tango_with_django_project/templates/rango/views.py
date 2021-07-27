fb76a17dc752826c7af7465048aa55bad6981bbb
Leroy030 <2529061l@student.gla.ac.uk>
2021-07-27 14:46:05 +0100

    Finish chapter4

--- tango_with_django_project/templates/rango/views.py
+++ tango_with_django_project/templates/rango/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 def about(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, './rango/about.html', context=context_dict)
+    return render(request, 'rango/about.html', context=context_dict)
     # return HttpResponse("<html>Rango says here is the about page.</br><a href='/rango/'>Index</a></html>")
 
 
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, './rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
